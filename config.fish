5d129e6178d8a165a104b71db058b1c58b366ed1
AK31437 <alban_knispel@freenet.de>
2026-09-05 13:36:30 +0200

    Revert "add aliases for managing scaling_governor to fish.conf"
    This reverts commit 99336250c8330ab4d7f8f0424737cc9f44b7477b.

--- config.fish
+++ config.fish
@@ -14,16 +14,5 @@ if status is-interactive
 
     fish_vi_key_bindings
 
-    # Aliases ----------------------------------------------------
-    
     alias v='nvim'
-
-    alias cpuago='cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor' # check available active scaling_governor
-    alias cpugov='cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor' # active scaling_governor
-    alias cpuper='echo performance | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor' # set scaling_governor to performance mode
-    alias cpupow='echo powersave | tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor' # set scaling_governor to powersave mode
-    alias cpufre='watch 'cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq'' # watch cpu frequency live
-
-    # ------------------------------------------------------------
-
 end
